cd72da41a5a1923b3052db310892c9651b818d23
Denis Kulikov <9933denis@gmail.com>
2024-12-09 21:37:55 +0300

    feat: add solution of sumArrays

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -42,8 +42,32 @@ function getIntervalArray(start, end) {
  *    sumArrays([10, 20, 30], [5, 10, 15]) => [15, 30, 45]
  *    sumArrays([-1, 0, 1], [1, 2, 3, 4]) => [0, 2, 4, 4]
  */
-function sumArrays(/* arr1, arr2 */) {
-  throw new Error('Not implemented');
+function sumArrays(arr1, arr2) {
+  let array;
+
+  if (arr1.length >= arr2.length) {
+    array = arr1.map((element, index) => {
+      let result;
+      if (arr2[index] !== undefined) {
+        result = element + arr2[index];
+      } else {
+        result = element;
+      }
+      return result;
+    });
+  } else {
+    array = arr2.map((element, index) => {
+      let result;
+      if (arr1[index] !== undefined) {
+        result = element + arr1[index];
+      } else {
+        result = element;
+      }
+      return result;
+    });
+  }
+
+  return array;
 }
 
 /**
